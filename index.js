435115f321b1932e14e350e2621338619c506f55
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2024-04-04 16:53:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -61,73 +61,82 @@ events.addEventListener("complaint", (event) => {
 async function handleComplaint(complaint) {
   console.log(complaint);
   const tempFolder = path.join(__dirname, "temp", `${complaint.id}`);
-  mkdir(tempFolder);
+  try {
+    mkdir(tempFolder);
 
-  await fs.promises.writeFile(path.join(tempFolder, "complaint.json"), JSON.stringify(complaint, null, 2));
+    await fs.promises.writeFile(path.join(tempFolder, "complaint.json"), JSON.stringify(complaint, null, 2));
 
-  let firstImagePath;
-  for (let i = 0; i < complaint.attachments.length; i++) {
-    const atc = complaint.attachments[i];
-    console.log("Downloading attachment", i + 1, "of", complaint.attachments.length, "for complaint", complaint.id);
-    let atcPath = path.join(tempFolder, atc.url.split("/").pop())
-    await download(`https://files.sikayetvar.com/complaint${atc.url}`, atcPath);
-    if (!firstImagePath && atc.mimeType == "photo") firstImagePath = atcPath;
+    let firstImagePath;
+    for (let i = 0; i < complaint.attachments.length; i++) {
+      const atc = complaint.attachments[i];
+      console.log("Downloading attachment", i + 1, "of", complaint.attachments.length, "for complaint", complaint.id);
+      let atcPath = path.join(tempFolder, atc.url.split("/").pop())
+      await download(`https://files.sikayetvar.com/complaint${atc.url}`, atcPath);
+      if (!firstImagePath && atc.mimeType == "photo") firstImagePath = atcPath;
+    }
+
+    const zipPath = path.join(tempFolder, "complaint.zip");
+    console.log(await new Promise(r => zip.zip(tempFolder, zipPath, r)));
+  } catch {
+    console.error("Error while handling complaint", complaint.id);
+    await fs.promises.rm(tempFolder, { recursive: true }).catch(console.error);
   }
 
-  const zipPath = path.join(tempFolder, "complaint.zip");
-  console.log(await new Promise(r => zip.zip(tempFolder, zipPath, r)));
-
   makeSureSent(async () => {
-    /** @type {import("discord.js").GuildTextBasedChannel} */
-    const channel = await client.channels.fetch(process.env.DISCORD_CHANNEL_ID);
+    try {
+      /** @type {import("discord.js").GuildTextBasedChannel} */
+      const channel = await client.channels.fetch(process.env.DISCORD_CHANNEL_ID);
 
-    const imgExt = firstImagePath ? path.extname(firstImagePath) : null;
-    const title = complaint.title ? `${complaint.title} ${complaint.relatedCompanies.map(i => i.name).join(" ")}`.toLowerCase() : "No title";
-    await channel.send({
-      content: title,
-      files: [
-        new Discord.AttachmentBuilder(zipPath).setName("complaint.zip"),
-        firstImagePath ? new Discord.AttachmentBuilder(firstImagePath, { name: `image${imgExt}` }) : null
-      ].filter(Boolean),
-      embeds: [{
-        color: 0x03e4b5,
-        title: complaint.title,
-        description: complaint.body.slice(0, 2000),
-        image: firstImagePath ? { url: `attachment://image${imgExt}` } : undefined,
-        author: {
-          name: complaint.member.shortName,
-          icon_url: `https://files.sikayetvar.com${complaint.member.picture}`,
-          url: `https://www.sikayetvar.com${complaint.member.url}`
-        },
-        timestamp: complaint.complainTime,
-        fields: [
-          complaint.attachments.length ? {
-            name: "Attachments",
-            value: complaint.attachments.map((a) => `[${a.url.split("/").pop().split(".").slice(1).join(".")}](https://files.sikayetvar.com/complaint${a.url})`).join(", ").slice(0, 1000),
-            inline: true
-          } : undefined,
-          complaint.relatedCompanies.length ? {
-            name: "Related Companies",
-            value: complaint.relatedCompanies.map(c => `[${c.name}](https://www.sikayetvar.com/${c.url})`).join(", ").slice(0, 1000),
-            inline: true
-          } : undefined,
-          complaint.grayListedWords.length ? {
-            name: "Gray Listed Words",
-            value: complaint.grayListedWords.join(", ").slice(0, 1000),
-            inline: true
-          } : undefined,
-          complaint.undefinedWords.length ? {
-            name: "Undefined Words",
-            value: complaint.undefinedWords.join(", ").slice(0, 1000),
-            inline: true
-          } : undefined,
+      const imgExt = firstImagePath ? path.extname(firstImagePath) : null;
+      const title = complaint.title ? `${complaint.title} ${complaint.relatedCompanies.map(i => i.name).join(" ")}`.toLowerCase() : "No title";
+      await channel.send({
+        content: title,
+        files: [
+          new Discord.AttachmentBuilder(zipPath).setName("complaint.zip"),
+          firstImagePath ? new Discord.AttachmentBuilder(firstImagePath, { name: `image${imgExt}` }) : null
         ].filter(Boolean),
-        footer: {
-          text: `Id: ${complaint.id} | Has ${complaint.attachments.length} attachments.`
-        }
-      }]
-    });
+        embeds: [{
+          color: 0x03e4b5,
+          title: complaint.title,
+          description: complaint.body.slice(0, 2000),
+          image: firstImagePath ? { url: `attachment://image${imgExt}` } : undefined,
+          author: {
+            name: complaint.member.shortName,
+            icon_url: `https://files.sikayetvar.com${complaint.member.picture}`,
+            url: `https://www.sikayetvar.com${complaint.member.url}`
+          },
+          timestamp: complaint.complainTime,
+          fields: [
+            complaint.attachments.length ? {
+              name: "Attachments",
+              value: complaint.attachments.map((a) => `[${a.url.split("/").pop().split(".").slice(1).join(".")}](https://files.sikayetvar.com/complaint${a.url})`).join(", ").slice(0, 1000),
+              inline: true
+            } : undefined,
+            complaint.relatedCompanies.length ? {
+              name: "Related Companies",
+              value: complaint.relatedCompanies.map(c => `[${c.name}](https://www.sikayetvar.com/${c.url})`).join(", ").slice(0, 1000),
+              inline: true
+            } : undefined,
+            complaint.grayListedWords.length ? {
+              name: "Gray Listed Words",
+              value: complaint.grayListedWords.join(", ").slice(0, 1000),
+              inline: true
+            } : undefined,
+            complaint.undefinedWords.length ? {
+              name: "Undefined Words",
+              value: complaint.undefinedWords.join(", ").slice(0, 1000),
+              inline: true
+            } : undefined,
+          ].filter(Boolean),
+          footer: {
+            text: `Id: ${complaint.id} | Has ${complaint.attachments.length} attachments.`
+          }
+        }]
+      });
 
+    } catch (e) {
+      console.error(e);
+    }
     setTimeout(async () => {
       await fs.promises.rm(tempFolder, { recursive: true }).catch(console.error);
     }, 5000);
